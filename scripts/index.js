dc6719d13faccd3bf8a6096a6f4f0148cee48ffb
Shubhanshu Tomar <57960301+tomshoo@users.noreply.github.com>
2024-01-11 16:01:01 +0530

    chore: unpolute global namespace

--- scripts/index.js
+++ scripts/index.js
@@ -17,18 +17,19 @@ window.onload = () => {
         }
     };
 
-    let src_link = document
-        .getElementsByClassName("note-footer")[0]
-        .getElementsByTagName("a")[0];
+    {
+        let src_link = document
+            .getElementsByClassName("note-footer")[0]
+            .getElementsByTagName("a")[0];
 
-    src_link.onmouseenter = (_ev) => {
-        let actual_link = src_link.getAttribute("href");
-        src_link.innerHTML = `<a href="${actual_link}">Source Code</a>`;
-        _ev.stopImmediatePropagation();
-    };
+        src_link.onmouseenter = (_ev) => {
+            let actual_link = src_link.getAttribute("href");
+            src_link.innerHTML = `<a href="${actual_link}" target="_blank">Source Code</a>`;
+        };
 
-    src_link.onmouseleave = () => {
-        let actual_link = src_link.getAttribute("href");
-        src_link.innerHTML = `<a href="${actual_link}">SRC</a>`;
-    };
+        src_link.onmouseleave = () => {
+            let actual_link = src_link.getAttribute("href");
+            src_link.innerHTML = `<a href="${actual_link}" target="_blank">SRC</a>`;
+        };
+    }
 };
